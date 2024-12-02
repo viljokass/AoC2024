c9a286330bf1fe9e64fc2fc7e71b31a352801602
viljokass <vili.karkkainen@outlook.com>
2024-12-02 18:24:57 +0200

    day 2 done

--- day_02/task_2/main.c
+++ day_02/task_2/main.c
@@ -14,10 +14,74 @@ int int_abs(int x) {
   return x;
 }
 
+// Counts the nums in line
+int count_nums(const char* line) {
+  int count = 0;
+  for ( int i = 0; i < 100; ++i ) {
+    if ( line[i] == '\n') {
+      count++;
+      break;
+    }
+    if ( line[i] == ' ' ) count++;
+  }
+  return count;
+}
+
+// Parse numbers from the text
+void parsenums(const char* line, int* nums) {
+  int index = 1;
+  nums[0] = atoi(line);
+  for (int i = 0; i < 100; ++i) {
+    if ( line[i] == '\n') {
+      break;
+    }
+    if ( line[i] == ' ' ) {
+      nums[index] = atoi(line + i + 1);
+      index++;
+    }
+  }
+}
+
+// Check if the line is safe if one is removed
+bool check_line_with_pardon(int* nums, int size, int pardon_index) {
+  enum direction dir = undef;
+
+  int start_index = 0;
+  if ( pardon_index == 0 ) start_index = 1;
+  int prev = nums[start_index];
+  int curr;
+
+  for ( int i = start_index + 1; i < size; ++i ) {
+    if ( i == pardon_index ) continue;
+    curr = nums[i];
+    if ( dir == undef ) {
+      if ( prev - curr < 0 ) dir = asc;
+      if ( prev - curr > 0 ) dir = desc;
+    }
+    if ( dir == asc ) {
+      if ( prev - curr > 0 ) return false;
+    }
+    if ( dir == desc ) {
+      if ( prev - curr < 0 )  return false;
+    }
+    if ( prev == curr ) return false;
+    if ( int_abs(prev - curr) > 3 ) return false;
+    prev = curr;
+  }
+  return true;
+}
+
 // Check a line
 bool is_line_safe(const char* line) {
-  // REDO THE LINE CHECKING
-  return true;
+  int numcount = count_nums(line);
+  int* nums = malloc(numcount * sizeof(int));
+  parsenums(line, nums);
+
+  bool safe = check_line_with_pardon(nums, numcount, -1);
+  for (int i = 0; i < numcount; ++i)
+    safe |= check_line_with_pardon(nums, numcount, i);
+  free(nums);
+  return safe;
 }
 
 int main(int argc, const char* argv[]) {
